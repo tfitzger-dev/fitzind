5c982365150036b96e97d2985b2504ea639a1f2c
Thomas Fitzgerald <tfitzger.dev@gmail.com>
2019-03-14 07:25:20 -0500

    Updated user serializer to prep for reg/login in client

--- job_track_api/serializers.py
+++ job_track_api/serializers.py
@@ -7,10 +7,14 @@ from job_track_api.models import Job, Task
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'jobs', 'tasks')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'password', 'jobs', 'tasks')
     jobs = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     tasks = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    #password = serializers.CharField(write_only=True)
 
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'], first_name=validated_data['first_name'], last_name=validated_data['last_name']);
+        return user
 
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
